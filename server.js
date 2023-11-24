2ebe3fd727717a00cd4f5df79154c70a1e7012da
laszloekovacs <laszloekovacs@gmail.com>
2023-11-24 16:51:52 +0100

    added auth params

--- server.js
+++ server.js
@@ -4,4 +4,22 @@ import { auth } from "express-openid-connect";
 const app = express();
 const port = 3000;
 
-app.use(auth({}));
+app.use(
+  auth({
+    /*
+    clientID:
+    clientSecret:
+    issuerBaseURL: 
+    baseURL:
+    secret:
+  */
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello" + JSON.stringify(req.oidc));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
