04e8c3c8e89348f0510691676ed5f50e0c3b9efb
Laszlo E Kovacs <laszloekovacs@gmail.com>
2023-11-24 20:52:31 +0100

    added profile

--- server.js
+++ server.js
@@ -1,25 +1,78 @@
-import express from "express";
-import { auth } from "express-openid-connect";
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const jwt = require('jsonwebtoken')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(
-  auth({
-    /*
-    clientID:
-    clientSecret:
-    issuerBaseURL: 
-    baseURL:
-    secret:
-  */
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/login', (req, res) => {
+  const qs = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: 'http://localhost:3000/callback',
+    scope: 'openid profile email',
   })
-);
 
-app.get("/", (req, res) => {
-  res.send("Hello" + JSON.stringify(req.oidc));
-});
+  res.redirect('https://github.com/login/oauth/authorize' + '?' + qs.toString())
+})
+
+app.get('/callback', async (req, res) => {
+  console.log(req.query)
+
+  const qs = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: req.query.code,
+  })
+
+  const response = await fetch(
+    'https://github.com/login/oauth/access_token' + '?' + qs.toString(),
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+  )
+
+  // get the access token
+  const data = await response.json()
+
+  // set the cookie with the access token
+  res.cookie('Bearer', data.access_token, {
+    httpOnly: false,
+    secure: false,
+    sameSite: 'strict',
+  })
+
+  res.redirect('/')
+})
+
+app.get('/profile', async (req, res) => {
+  if (!req.cookies.Bearer) {
+    res.redirect('/login')
+    return
+  }
+
+  const response = await fetch('https://api.github.com/user', {
+    headers: {
+      Authorization: `Bearer ${req.cookies.Bearer}`,
+    },
+  })
+
+  const data = await response.json()
+
+  res.send(data)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening: http://localhost:${port}`)
+})
